0b0c642dacdc648afbaafa9502119b5cf0f4767f
sajir-dev <62017128+sajir-dev@users.noreply.github.com>
2020-04-11 21:41:17 +0530

    Add files via upload

---
+++ 028_greatest.js
@@ -0,0 +1,4 @@
+greatestofthree = (a, b) => {
+  return a > b ? a : b;
+};
+console.log(greatest(201, 201));
---
+++ 043_fractionAddition.js
@@ -0,0 +1,8 @@
+/* fractionAddition = (a, b) => {
+  return Math.add(math.fraction(a)+math.fraction(b));
+};
+*/
+
+//console.log(fractionAddition('1/3', '5/6'));
+
+console.log(simplify('3 + 2 / 4').toString())
